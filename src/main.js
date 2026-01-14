78960e7f3c0756102b5cb3f2052d7b84c6002410
Petrik Diak <diak@petrik.hu>
2026-01-14 11:21:08 +0100

    Létre hoztam egy prototipust melyben meg lehet adni a kért 3db adatot

--- src/main.js
+++ src/main.js
@@ -5,7 +5,20 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-  <h1>hello</h1>
+ <form action="/action_page.php">
+
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+
+    <label for="email">Email :</label><br>
+  <input type="text" id="email" name="email" value=""><br><br>
+
+  <input type="submit" value="Submit">
+</form> 
+
 
   </div>
 `
