d2df0b74e3f528f4ccf9b34e5c109d515b381b49
Petrik Diak <diak@petrik.hu>
2026-01-14 12:09:37 +0100

    Footer létrehozva, Form renoválása, Kódók javitása, Hibák keresése

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-      const form = document.getElementById('regForm');
-      const messageDiv = document.getElementById('message');
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('regForm');
+  const messageDiv = document.getElementById('message');
 
-      form.addEventListener('submit', function(e) {
-        e.preventDefault(); // Megakadályozza az űrlap tényleges elküldését
-        form.reset(); // Űrlap törlése
-        messageDiv.innerHTML = '<div class="alert alert-success" role="alert">Sikeres regisztráció!</div>';
-      });
+  form.addEventListener('submit', function(e) {
+    e.preventDefault(); 
+    form.reset(); 
+    messageDiv.innerHTML = '<div class="alert alert-success" role="alert">Sikeres regisztráció!</div>';
+  });
+});
